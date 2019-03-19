f69ce20b26c2d162f291bb987d8f49914ba7536f
sundowndev <raphael@crvx.fr>
2019-03-19 17:04:08 +0100

    Support for Google search API key

---
+++ config.example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#
+# @name   : PhoneInfoga - Phone numbers OSINT tool
+# @url    : https://github.com/sundowndev
+# @author : Raphael Cerveaux (sundowndev)
+
+google_api_key=''
+google_cx_id=''
