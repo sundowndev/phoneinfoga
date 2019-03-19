df4f1f6e08ed186ec7cd56dc56a350f9bbf3907b
sundowndev <raphael@crvx.fr>
2019-03-19 18:37:32 +0100

    Example script (typo)

--- examples/generate.sh
+++ examples/generate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 {
     # command which may fail and give an error 
     scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
@@ -13,12 +11,10 @@
     python3 $scriptDir/../phoneinfoga.py -i $scriptDir/input.txt -o $scriptDir/output_from_input.txt -s any
 
     python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -s numverify -o $scriptDir/output_single.txt
-    
-    echo "Test script executed."
-    
-    python4 
 } || {
        # command which should be run instead of the above failing command
        echo "Command exited with an error."
        exit 1
 }
+
+echo "Test script executed."
