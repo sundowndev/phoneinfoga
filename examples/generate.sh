b05d72915bcc9e1210d2ef1d8f309ad4b42aa07d
sundowndev <raphael@crvx.fr>
2019-03-19 18:34:27 +0100

    Handle example script errors

--- examples/generate.sh
+++ examples/generate.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -h
 
-python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -s any --no-ansi
+{
+    # command which may fail and give an error 
+    scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-python3 $scriptDir/../phoneinfoga.py -i $scriptDir/input.txt -o $scriptDir/output_from_input.txt -s any
+    python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -h
 
-python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -s numverify -o $scriptDir/output_single.txt
+    python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -s any --no-ansi
 
-echo "Test script executed."
+    python3 $scriptDir/../phoneinfoga.py -i $scriptDir/input.txt -o $scriptDir/output_from_input.txt -s any
+
+    python3 $scriptDir/../phoneinfoga.py -n "+86 591 2284 8571" -s numverify -o $scriptDir/output_single.txt
+    
+    echo "Test script executed."
+    
+    python4 
+} || {
+       # command which should be run instead of the above failing command
+       echo "Command exited with an error."
+       exit 1
+}
