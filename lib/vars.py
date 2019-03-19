2d77b5ccad743ead3fd00696f873907bebfb75be
sundowndev <raphael@crvx.fr>
2019-03-19 09:32:36 +0100

    Delete useless lib files

--- lib/vars.py
+++
@@ -1,6 +0,0 @@
-number = ''  # Full number format; e.g: 3312345678
-localNumber = ''  # Local number format; e.g: 06 12 34 56 78
-internationalNumber = ''  # International number format; e.g: +33 6 12 34 56 78
-numberCountryCode = ''  # Dial code; e.g: 33
-numberCountry = ''  # Country; e.g: fr
-
