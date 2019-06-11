2f5710e5c3e5d34fc05816c0d50b98430e55a986
sundowndev <raphael@crvx.fr>
2019-06-11 13:22:00 +0200

    bump version

--- lib/banner.py
+++ lib/banner.py
@@ -5,7 +5,7 @@
 # @url    : https://github.com/sundowndev
 # @author : Raphael Cerveaux (sundowndev)
 
-__version__ = 'v1.6.8'
+__version__ = 'v1.10.9'
 
 def banner():
     print("    ___ _                       _____        __                   ")
