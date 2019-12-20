3f462fe11128bd80c1611c8639b216c58059dd5d
sundowndev <raphael@crvx.fr>
2019-12-20 15:57:56 +0100

    test(lib): banner

--- tests/lib/banner.py
+++ tests/lib/banner.py
@@ -11,11 +11,11 @@ from unittest.mock import patch
 from lib.banner import banner, __version__
 
 
-class TestAnswer(unittest.TestCase):
+class TestBanner(unittest.TestCase):
     def test_type(self):
         self.assertIs(type(__version__), type("str"))
 
-    @patch('builtins.print')
+    @patch("builtins.print")
     def test_banner_prints(self, printMock):
         banner()
 
