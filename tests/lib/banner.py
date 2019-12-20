707e436201322816856e43b2222225b46ea17ff9
sundowndev <raphael@crvx.fr>
2019-12-20 15:32:46 +0100

    test(lib): banner

--- tests/lib/banner.py
+++ tests/lib/banner.py
@@ -6,6 +6,7 @@
 # @author : Raphael Cerveaux (sundowndev)
 
 import unittest
+from unittest.mock import patch
 
 from lib.banner import banner, __version__
 
@@ -13,3 +14,9 @@ from lib.banner import banner, __version__
 class TestAnswer(unittest.TestCase):
     def test_type(self):
         self.assertIs(type(__version__), type("str"))
+
+    @patch('builtins.print')
+    def test_banner_prints(self, printMock):
+        banner()
+
+        self.assertEqual(printMock.call_count, 9)
