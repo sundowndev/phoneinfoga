e9258323eeb1a184eb49f022653b0517a119fc28
sundowndev <raphael@crvx.fr>
2020-03-18 10:28:34 +0000

    refactor(api): validator
    Only accepts phone number as numeric value

--- api/validators.go
+++ api/validators.go
@@ -1,8 +1,9 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"github.com/sundowndev/phoneinfoga/pkg/utils"
 )
 
 // JSONResponse is the default API response type
@@ -11,16 +12,20 @@ type JSONResponse struct {
 	Error   string `json:"error"`
 }
 
-// type scanURL struct {
-// 	Number uint `uri:"number" binding:"required,min=1,max=999679368229"`
-// }
+type scanURL struct {
+	Number uint `uri:"number" binding:"required,min=2"`
+}
 
 // ValidateScanURL validates scan URLs
 func ValidateScanURL(c *gin.Context) {
-	number := c.Param("number")
+	var v scanURL
 
-	if valid := utils.IsValid(number); !valid {
-		c.JSON(500, errorResponse("The number is not valid"))
-		c.Abort()
+	if err := c.ShouldBindUri(&v); err != nil {
+		errorHandling(c, err.Error())
 	}
+}
+
+func errorHandling(c *gin.Context, msg string) {
+	c.JSON(http.StatusBadRequest, JSONResponse{Success: false, Error: msg})
+	c.Abort()
 }
