497795db8fa5a9186a60f7b76674cbde08aaaf84
sundowndev <raphael@crvx.fr>
2020-03-18 16:17:42 +0000

    chore: fix lint errors

--- client/src/App.vue
+++ client/src/App.vue
@@ -23,8 +23,16 @@
           <b-navbar-nav class="ml-auto">
             <b-collapse id="nav-collapse" is-nav>
               <b-navbar-nav>
-                <b-nav-item href="https://sundowndev.github.io/PhoneInfoga/resources/" target="_blank">Resources</b-nav-item>
-                <b-nav-item href="https://sundowndev.github.io/PhoneInfoga/" target="_blank">Documentation</b-nav-item>
+                <b-nav-item
+                  href="https://sundowndev.github.io/PhoneInfoga/resources/"
+                  target="_blank"
+                  >Resources</b-nav-item
+                >
+                <b-nav-item
+                  href="https://sundowndev.github.io/PhoneInfoga/"
+                  target="_blank"
+                  >Documentation</b-nav-item
+                >
               </b-navbar-nav>
             </b-collapse>
           </b-navbar-nav>
