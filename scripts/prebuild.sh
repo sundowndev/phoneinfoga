2671eaad41772a6532c450e07163ddb4cee03fc5
Raphaël <16480203+sundowndev@users.noreply.github.com>
2020-03-19 16:07:47 +0000

    fix(ci): prebuild script (#232)
    in order to fix goreleaser job

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+# Build static assets before compiling software.
+# This script is intended to be run from an UNIX system.
+# If you don't have nodejs, yarn or curl, please install it manually.
+# Automatic installation will use apt independently from your OS.
+
 if !(hash curl 2>/dev/null); then
         echo "Curl is needed to run prebuild."
         exit 1;
 fi;
 
-if !(hash yarn 2>/dev/null); then
-        curl -o- -L https://yarnpkg.com/install.sh | bash
+if !(hash yarn 2>/dev/null) || !(hash node 2>/dev/null); then
+        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+        apt-get update
+        apt-get install yarn nodejs -y
 fi;
 
 echo ">> Building web client"
 (cd client && yarn && yarn build)
-
-echo ">> Installing packr v2"
-go get -u -v github.com/gobuffalo/packr/v2/packr2
 
 echo ">> Building static assets"
 $GOPATH/bin/packr2
