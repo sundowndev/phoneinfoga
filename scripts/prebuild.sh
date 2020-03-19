8b6a3102c653ea00791944ebfc25b4e4e6f79f50
Raphaël <16480203+sundowndev@users.noreply.github.com>
2020-03-19 11:55:29 +0000

    refactor(scripts): prebuild

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -5,15 +5,19 @@ if !(hash curl 2>/dev/null); then
         exit 1;
 fi;
 
+if !(hash node 2>/dev/null); then
+        VERSION=v12.16.1
+        DISTRO=linux-x64
+        sudo mkdir -p /usr/local/lib/nodejs
+        sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+fi;
+
 if !(hash yarn 2>/dev/null); then
         curl -o- -L https://yarnpkg.com/install.sh | bash
 fi;
 
 echo ">> Building web client"
-(cd client && yarn && yarn build)
-
-echo ">> Installing packr v2"
-go get -u -v github.com/gobuffalo/packr/v2/packr2
+(cd client && $HOME/.yarn/bin/yarn && $HOME/.yarn/bin/yarn build)
 
 echo ">> Building static assets"
 $GOPATH/bin/packr2
