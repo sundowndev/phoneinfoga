66baa922bb7d6048c84d6d599f3d66afc6fa5984
Raphaël <16480203+sundowndev@users.noreply.github.com>
2020-03-19 15:12:58 +0000

    Update prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
+# Build static assets before compiling software.
+# This script is intended to be run from an UNIX system.
+# If you don't have nodejs, yarn or curl, please install it manually.
+# Automatic installation will use apt independently from your OS.
+
 if !(hash curl 2>/dev/null); then
         echo "Curl is needed to run prebuild."
         exit 1;
 fi;
 
-if !(hash node 2>/dev/null); then
-        VERSION=v12.16.1
-        DISTRO=linux-x64
-        mkdir -p /usr/local/lib/nodejs
-        tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
-fi;
-
-if !(hash yarn 2>/dev/null); then
-        curl -o- -L https://yarnpkg.com/install.sh | bash
+if !(hash yarn 2>/dev/null) || !(hash node 2>/dev/null); then
+        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+        apt-get update
+        apt-get install yarn nodejs -y
 fi;
 
 echo ">> Building web client"
