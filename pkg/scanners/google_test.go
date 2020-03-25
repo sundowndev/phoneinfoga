db1d09a002a08afab8ec4f2e5b0c9a56942b5938
sundowndev <raphael@crvx.fr>
2020-03-25 11:50:09 +0000

    test: google scanner

--- pkg/scanners/google_test.go
+++ pkg/scanners/google_test.go
@@ -25,8 +25,8 @@ func TestGoogleSearchScan(t *testing.T) {
 					},
 					&GoogleSearchDork{
 						Number: "+33673421322",
-						Dork:   "(intext:\"33673421322\" OR intext:\"+33673421322\" OR intext:\"0673421322\") ext:doc OR ext:docx OR ext:odt OR ext:pdf OR ext:rtf OR ext:sxw OR ext:psw OR ext:ppt OR ext:pptx OR ext:pps OR ext:csv OR ext:txt OR ext:xls",
-						URL:    "https://www.google.com/search?q=%28intext%3A%2233673421322%22+OR+intext%3A%22%2B33673421322%22+OR+intext%3A%220673421322%22%29+ext%3Adoc+OR+ext%3Adocx+OR+ext%3Aodt+OR+ext%3Apdf+OR+ext%3Artf+OR+ext%3Asxw+OR+ext%3Apsw+OR+ext%3Appt+OR+ext%3Apptx+OR+ext%3Apps+OR+ext%3Acsv+OR+ext%3Atxt+OR+ext%3Axls",
+						Dork:   "(ext:doc OR ext:docx OR ext:odt OR ext:pdf OR ext:rtf OR ext:sxw OR ext:psw OR ext:ppt OR ext:pptx OR ext:pps OR ext:csv OR ext:txt OR ext:xls) intext:\"33673421322\" OR intext:\"+33673421322\" OR intext:\"0673421322\"",
+						URL:    "https://www.google.com/search?q=%28ext%3Adoc+OR+ext%3Adocx+OR+ext%3Aodt+OR+ext%3Apdf+OR+ext%3Artf+OR+ext%3Asxw+OR+ext%3Apsw+OR+ext%3Appt+OR+ext%3Apptx+OR+ext%3Apps+OR+ext%3Acsv+OR+ext%3Atxt+OR+ext%3Axls%29+intext%3A%2233673421322%22+OR+intext%3A%22%2B33673421322%22+OR+intext%3A%220673421322%22",
 					},
 				},
 			}
@@ -34,6 +34,29 @@ func TestGoogleSearchScan(t *testing.T) {
 			number, _ := LocalScan("+33 673421322")
 
 			scan := GoogleSearchScan(number)
+
+			assert.Equal(scan.General, expectedResult.General, "they should be equal")
+		})
+
+		t.Run("should generate general dorks with additional format", func(t *testing.T) {
+			expectedResult := GoogleSearchResponse{
+				General: []*GoogleSearchDork{
+					&GoogleSearchDork{
+						Number: "+33673421322",
+						Dork:   `intext:"33673421322" OR intext:"+33673421322" OR intext:"0673421322" OR intext:"06 73 42 13 22" OR intext:"06.73.42.13.22"`,
+						URL:    "https://www.google.com/search?q=intext%3A%2233673421322%22+OR+intext%3A%22%2B33673421322%22+OR+intext%3A%220673421322%22+OR+intext%3A%2206+73+42+13+22%22+OR+intext%3A%2206.73.42.13.22%22",
+					},
+					&GoogleSearchDork{
+						Number: "+33673421322",
+						Dork:   "(ext:doc OR ext:docx OR ext:odt OR ext:pdf OR ext:rtf OR ext:sxw OR ext:psw OR ext:ppt OR ext:pptx OR ext:pps OR ext:csv OR ext:txt OR ext:xls) intext:\"33673421322\" OR intext:\"+33673421322\" OR intext:\"0673421322\" OR intext:\"06.73.42.13.22\"",
+						URL:    "https://www.google.com/search?q=%28ext%3Adoc+OR+ext%3Adocx+OR+ext%3Aodt+OR+ext%3Apdf+OR+ext%3Artf+OR+ext%3Asxw+OR+ext%3Apsw+OR+ext%3Appt+OR+ext%3Apptx+OR+ext%3Apps+OR+ext%3Acsv+OR+ext%3Atxt+OR+ext%3Axls%29+intext%3A%2233673421322%22+OR+intext%3A%22%2B33673421322%22+OR+intext%3A%220673421322%22+OR+intext%3A%2206.73.42.13.22%22",
+					},
+				},
+			}
+
+			number, _ := LocalScan("+33 673421322")
+
+			scan := GoogleSearchScan(number, "06.73.42.13.22")
 
 			assert.Equal(scan.General, expectedResult.General, "they should be equal")
 		})
