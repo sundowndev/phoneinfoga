1387aff4dc96946f977a816a89fa8de525cb0dc7
sundowndev <raphael@crvx.fr>
2020-03-25 11:30:20 +0000

    test: ovh scanner

--- pkg/scanners/ovh_test.go
+++ pkg/scanners/ovh_test.go
@@ -81,4 +81,30 @@ func TestOVHScanner(t *testing.T) {
 
 		assert.Equal(gock.IsDone(), true, "there should have no pending mocks")
 	})
+
+	t.Run("should not find country code on OVH", func(t *testing.T) {
+		defer gock.Off() // Flush pending mocks after test execution
+
+		gock.New("https://api.ovh.com").
+			Get("/1.0/telephony/number/detailedZones").
+			MatchParam("country", "us").
+			Reply(200).
+			JSON(map[string]string{
+				"message": "[country] Given data (us) does not belong to the NumberCountryEnum enumeration",
+			})
+
+		number, _ := LocalScan("+1 718-521-2994")
+
+		result, err := OVHScan(number)
+
+		assert.Equal(err, nil, "should not be errored")
+		assert.Equal(result, &OVHScannerResponse{
+			Found:       false,
+			NumberRange: "",
+			City:        "",
+			ZipCode:     "",
+		}, "they should be equal")
+
+		assert.Equal(gock.IsDone(), true, "there should have no pending mocks")
+	})
 }
