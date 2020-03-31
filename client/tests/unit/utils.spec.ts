53094358c26aa98c79810a5c8457c02624985352
buffay0 <62791838+buffay0@users.noreply.github.com>
2020-03-31 20:34:15 +0300

    Update utils.spec.ts

--- client/tests/unit/utils.spec.ts
+++ client/tests/unit/utils.spec.ts
@@ -3,16 +3,16 @@ import { formatNumber, isValid } from "../../src/utils";
 describe("src/utils", () => {
   describe("#formatNumber", () => {
     it("should format phone number", () => {
-      expect(formatNumber("+1 (555) 444-3333")).toBe("15554443333");
-      expect(formatNumber("1/(555)_444-3333")).toBe("15554443333");
+      expect(formatNumber("+254 (74_370-6303")).toBe("254743706303");
+      expect(formatNumber("1/(254)_7437-06303")).toBe("254743706303");
     });
   });
 
   describe("#isValid", () => {
     it("should check if number is valid", () => {
-      expect(isValid("+1/(555)_444-3333")).toBe(true);
-      expect(isValid("1 555 4443333")).toBe(true);
-      expect(isValid("this1 555 4443333")).toBe(false);
+      expect(isValid("+254/(743)_706-303")).toBe(true);
+      expect(isValid("254 743 706303")).toBe(true);
+      expect(isValid("this254 743 706303")).toBe(false);
     });
   });
 });
