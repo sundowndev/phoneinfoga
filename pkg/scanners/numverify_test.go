b59d91027a6a966309fda2e0864dec6370f0bb2d
sundowndev <raphael@crvx.fr>
2020-05-17 16:48:40 +0200

    test(scanners): numverify
    Testing error handling when empty response is returned by Numverify's API, causing an EOF error.

--- pkg/scanners/numverify_test.go
+++ pkg/scanners/numverify_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	gock "gopkg.in/h2non/gock.v1"
+	"gopkg.in/sundowndev/phoneinfoga.v2/pkg/utils"
 )
 
 func TestNumverifyScanner(t *testing.T) {
@@ -39,9 +40,9 @@ func TestNumverifyScanner(t *testing.T) {
 			Reply(200).
 			JSON(expectedResult)
 
-		result, err := NumverifyScan(number)
+		result, err := numverifyScanCLI(utils.LoggerService, number)
 
-		assert.Equal(err, nil, "they should be equal")
+		assert.Nil(err, "they should be equal")
 		assert.Equal(result, &expectedResult, "they should be equal")
 
 		assert.Equal(gock.IsDone(), true, "there should have no pending mocks")
@@ -76,10 +77,33 @@ func TestNumverifyScanner(t *testing.T) {
 			Reply(200).
 			JSON(expectedResult)
 
-		result, err := NumverifyScan(number)
+		result, err := numverifyScanCLI(utils.LoggerService, number)
 
-		assert.Equal(err, nil, "they should be equal")
+		assert.Nil(err, "they should be equal")
 		assert.Equal(result, &expectedResult, "they should be equal")
+
+		assert.Equal(gock.IsDone(), true, "there should have no pending mocks")
+	})
+
+	t.Run("should return empty response and handle error properly", func(t *testing.T) {
+		defer gock.Off() // Flush pending mocks after test execution
+
+		number, _ := LocalScan("+123456789")
+
+		gock.New("http://numverify.com").
+			Get("/").
+			Reply(200).BodyString(`<html><body><input type="hidden" name="scl_request_secret" value="secret"/></body></html>`)
+
+		gock.New("https://numverify.com").
+			Get("/php_helper_scripts/phone_api.php").
+			MatchParam("secret_key", "7ccde16e862dfe7681297713e9e9cadb").
+			MatchParam("number", number.International).
+			Reply(200)
+
+		result, err := numverifyScanCLI(utils.LoggerService, number)
+
+		assert.EqualError(err, "EOF", "they should be equal")
+		assert.Nil(result, "they should be equal")
 
 		assert.Equal(gock.IsDone(), true, "there should have no pending mocks")
 	})
