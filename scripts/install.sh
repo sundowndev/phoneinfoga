75a01193a51c6cd984892931fb3b95b68a25da25
Siddharth Reddy Timmareddy Gari <sidd_tim@outlook.com>
2020-09-23 22:00:57 +0530

    Create install.sh
    Fixes #416 .

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+os=`echo "$(uname -s)_$(uname -m)"`
+if [ $os == "Linux_x86_64" ] || [ $os == "Linux_armv6" ] || [ $os == "Windows_x86_64" ] || [ $os == "Darwin_x86_64" ] || [ $os == "Linux_arm64" ] || [ $os == "Windows_i386" ] || [ $os == "Linux_i386" ]]
+then
+        echo Installing Phoneinfoga.........
+        wget "https://github.com/sundowndev/phoneinfoga/releases/download/v2.3.2/phoneinfoga_$(uname -s)_$(uname -m).tar.gz"
+        tar xfv "phoneinfoga_$(uname -s)_$(uname -m).tar.gz"
+        mv ./phoneinfoga /usr/bin/phoneinfoga
+        echo Installation completed  Successfully.
+else
+        echo Your OS/arch is not Supported.
+fi
