dc53070d865b42bd4328cb47419bbe1d37bdfc29
Siddharth Reddy Timmareddy Gari <sidd_tim@outlook.com>
2020-09-24 22:55:14 +0530

    Update install.sh
    Bash Script to install in Linux.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 os=`echo "$(uname -s)_$(uname -m)"`
-if [ $os == "Linux_x86_64" ] || [ $os == "Linux_armv6" ] || [ $os == "Windows_x86_64" ] || [ $os == "Darwin_x86_64" ] || [ $os == "Linux_arm64" ] || [ $os == "Windows_i386" ] || [ $os == "Linux_i386" ]]
+if [ $os == "Linux_x86_64" ] || [ $os == "Linux_armv6" ] || [ $os == "Darwin_x86_64" ] || [ $os == "Linux_arm64" ] || [ $os == "Linux_i386" ]]
 then
         echo Installing Phoneinfoga.........
         wget "https://github.com/sundowndev/phoneinfoga/releases/download/v2.3.2/phoneinfoga_$(uname -s)_$(uname -m).tar.gz"
