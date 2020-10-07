aedbcdb7e22171ab9c9a11827863938ddb58f5b9
Siddharth Reddy <sidd_tim@outlook.com>
2020-10-08 00:08:57 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-os=`echo "$(uname -s)_$(uname -m)"`
+
+os="$(uname -s)_$(uname -m)"
+
 if [ $os == "Linux_x86_64" ] || [ $os == "Linux_armv6" ] || [ $os == "Darwin_x86_64" ] || [ $os == "Linux_arm64" ] || [ $os == "Linux_i386" ]]
 then
-        echo Installing Phoneinfoga.........
-        wget "https://github.com/sundowndev/phoneinfoga/releases/download/v2.3.2/phoneinfoga_$(uname -s)_$(uname -m).tar.gz"
-        tar xfv "phoneinfoga_$(uname -s)_$(uname -m).tar.gz"
-        mv ./phoneinfoga /usr/bin/phoneinfoga
-        echo Installation completed  Successfully.
+	echo "Installing Phoneinfoga........."
+	phoneinfoga_version=$(curl -s https://api.github.com/repos/sundowndev/PhoneInfoga/releases/latest | grep tag_name | cut -d '"' -f 4)
+	wget "https://github.com/sundowndev/phoneinfoga/releases/download/$phoneinfoga_version/phoneinfoga_$os.tar.gz"
+	tar xfv "phoneinfoga_$os.tar.gz"
+	mv ./phoneinfoga /usr/bin/phoneinfoga
+	rm -rf phoneinfoga_$os.tar.gz
+	echo "Installation completed Successfully."
+
 else
-        echo Your OS/arch is not Supported.
+	echo "Your OS/arch is not Supported."
 fi
-exit 
+
+exit 0
